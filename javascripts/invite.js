1e3fd88396c03e9c384d6b84c67ebe4d48126287
bidengraphics <63020598+bidengraphics@users.noreply.github.com>
2020-10-31 01:10:53 -0400

    Update invite.js

--- javascripts/invite.js
+++ javascripts/invite.js
@@ -64,7 +64,7 @@ $(function() {
       obj
     );
     $('<img>', {
-      src: 'invite/InviteYourFriends_4Days.png',
+      src: 'invite/InviteYourFriends_3Days.png',
     }).watermark(config);
   }
 
